71e75cbbe66200a4f612d362e28f73248eac3034
Tazshelby2016 <tazshelby2016@gmail.com>
2025-02-25 17:03:52 +0000

    Use <Fragment> not <div>

--- forager/components/BasicImage.jsx
+++ forager/components/BasicImage.jsx
@@ -4,10 +4,10 @@ function BasicImage({height, width, hazard, alt, src}){
     // Will do CSS magic trick later
     if (hazard){
         return (
-            <div>
+            <>
                 <svg src="././public/icons/icon_warning.svg"></svg>
                 <img alt={alt} height={height} width={width}/>
-            </div>
+            </>
         );
     }
     else{
