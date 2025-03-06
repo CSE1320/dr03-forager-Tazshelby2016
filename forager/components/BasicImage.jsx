eb7acec59e07f97c5d2949de103af66f39e6cde9
Tazshelby2016 <tazshelby2016@gmail.com>
2025-03-06 17:04:24 +0000

    Attempted improvements to BasicImage

--- forager/components/BasicImage.jsx
+++ forager/components/BasicImage.jsx
@@ -1,23 +1,29 @@
 import React from 'react';
 
-function BasicImage({height=0, width=0, hazard=false, alt="You need to replace me.", src}){
+function BasicImage({height=0, width=0, hazard=false, alt="You need to replace me.", src=""}){
     // Will do CSS magic trick later
+    const simpleReturn = "<img alt=" + alt + " height=" + height + " width=" + width + "/>";
+    const iconReturn = "<><svg src=\"././public/icons/icon_warning.svg\"></svg>" + simpleReturn + "</>";
+
+    const fixPath = "\"" + src + "\""
+gi
     if (hazard){
         return (
-            <>
+            <div>
                 <svg src="././public/icons/icon_warning.svg"></svg>
-                <img alt={alt} height={height} width={width}/>
-            </>
+                <img src={src} alt={alt} height={height} width={width}/>
+            </div>
         );
     }
     else{
         return (
-            <>
-                <img alt={alt} height={height} width={width}/>
-            </>
+            <div>
+                <img src={src} alt={alt} height={height} width={width}/>
+            </div>
         );
     }
 
+    //return hazard? iconReturn:simpleReturn;
 }
 
 export default BasicImage;
--- forager/components/MockCamera.jsx
+++ forager/components/MockCamera.jsx
@@ -4,7 +4,7 @@ import BasicImage from '@/components/BasicImage'
 const MockCamera = () =>{
     return (
         <>
-            <BasicImage height="100px" width="100px" alt="testing"/>
+            <BasicImage height="100px" width="100px" alt="testing" src="././public/image1.png"/>
         </>
     );
 }
