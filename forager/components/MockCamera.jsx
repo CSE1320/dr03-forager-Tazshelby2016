6534cb8d7a9100cc257f839df23474c8af111f99
Tazshelby2016 <tazshelby2016@gmail.com>
2025-03-07 17:40:55 +0000

    Try to fix component

--- forager/components/MockCamera.jsx
+++ forager/components/MockCamera.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import BasicImage from '@/components/BasicImage'
 
+
 const MockCamera = () =>{
     return (
         <>
-            <BasicImage height="100px" width="100px" alt="testing" src="././public/image1.png"/>
+          <BasicImage height="100px" width="100px" alt="testing" src="/public/image1.png" hazard="true" />
         </>
     );
 }
